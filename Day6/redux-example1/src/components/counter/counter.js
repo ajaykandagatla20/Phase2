84024a383d0631523d14a1a4f98c748e7fd5e266
Ajay Kumar Kandagatla <111872379+ajaykandagatla20@users.noreply.github.com>
2025-03-16 12:48:43 +0530

    day6

---
+++ Day6/redux-example1/src/components/counter/counter.js
@@ -0,0 +1,24 @@
+import { useDispatch, useSelector } from "react-redux";
+import { increment,decrement,power } from "../reducers/CounterReduces";
+
+const Counter = () => {
+    const count = useSelector((state) => state.count);
+    
+    const dispatch = useDispatch()
+
+    return(
+        <div>
+            Count is : <b> {count}</b> <hr/>
+            <button onClick={() => dispatch(increment())}>
+                Increment 
+            </button> 
+            <button onClick={() => dispatch(decrement())}>
+                Decrement 
+            </button>
+            <button onClick={() => dispatch(power())}>
+                Power
+            </button>
+        </div>
+    )
+ }
+ export default Counter;
