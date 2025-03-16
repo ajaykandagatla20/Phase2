89a98839696dba930c76d55fec7238fc0aad092a
Ajay Kumar Kandagatla <111872379+ajaykandagatla20@users.noreply.github.com>
2025-03-16 12:56:10 +0530

    fromikexample

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
+import logo from './logo.svg';
 import './App.css';
-import First from './components/first/First';
-import Second from './components/second/Second';
-import Third from './components/third/Third';
-import Fourth from './components/fourth/Fourth';
-import ButtonEx from './components/buttonex/ButtonEx';
-import Fifth from './components/fifth/Fifth';
-import Counter from './components/counter/Counter';
-
-import { BrowserRouter,Routes,Route,Link } from 'react-router-dom';
-import Menu from './components/menu/Menu';
+import First from './components/first';
 
 function App() {
   return (
     <div className="App">
-     <h1>Introduction to functional component... </h1><br/><br/>
-     <BrowserRouter>
-   
-       <Routes>
-        <Route path="/" element={<Menu />}/> 
-        <Route path="/First" element={<First />}/>
-         <Route path="/Second" element={<Second />} />
-        <Route path="/Third" element={<Third firstName="Ajay" lastName="Kandagatla" company="Wipro" />}/>
-        <Route path="/Fourth" element={<Fourth/>}/>
-        
-        <Route path="/Fifth" element={<Fifth />}/> 
-
-        <Route path="/ButtonEx" element={<ButtonEx />}/>
-
-        <Route path="/Counter" element={<Counter/>}/>
-
-      </Routes>
-      </BrowserRouter> 
-      <br/><br/><br/><br/>
+      <First/>
     </div>
   );
 }
