25da9637b16c18882e8b27e14921ea9a4fe8d1c0
Ajay Kumar Kandagatla <111872379+ajaykandagatla20@users.noreply.github.com>
2025-03-17 12:57:27 +0530

    hi this is ajay

---
+++ hooksexample/src/excomponents/useStateExample.js
@@ -0,0 +1,15 @@
+// import React,{useState} from 'react';
+
+// const Counter=()=>{
+//     const[count,setCount]=useState(0);
+//     console.log("counter value is: ",{count});
+
+//     return(
+//         <div>
+//             <h1>Initial count value is : {count}</h1>
+//             <button onclick={()=>setCount(count+1)}>click here!</button>
+
+//         </div>
+//     );
+// }
+// export default Counter;
